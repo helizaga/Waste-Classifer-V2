e93cc4932b6dc8c8b5a04178621540a041574837
helizaga <tom.elizaga@gmail.com>
2021-03-10 22:18:02 -0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
