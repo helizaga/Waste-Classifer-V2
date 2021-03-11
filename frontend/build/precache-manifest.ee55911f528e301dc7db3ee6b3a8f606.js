fdd6d3ec5cc5426a9920ae29b0e56582e636e8ac
helizaga <tom.elizaga@gmail.com>
2021-03-11 00:41:08 -0800

    update

---
+++ frontend/build/precache-manifest.ee55911f528e301dc7db3ee6b3a8f606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "693ac5a65d4c6ed28cd5",
+    "url": "/static/css/main.f96fbc61.chunk.css"
+  },
+  {
+    "revision": "693ac5a65d4c6ed28cd5",
+    "url": "/static/js/main.693ac5a6.chunk.js"
+  },
+  {
+    "revision": "e2f06ff8fbc1064a0aba",
+    "url": "/static/js/1.e2f06ff8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a909d0a5403f1f128415f2d4738ea48d",
+    "url": "/index.html"
+  }
+];
