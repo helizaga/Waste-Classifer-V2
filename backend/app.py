08d9325f0dd6e056b54514dc77f643ba1caeb75c
helizaga <tom.elizaga@gmail.com>
2021-03-10 21:55:13 -0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ import tensorflow.compat.v1 as tf
 import requests
 
 #app = Flask(__name__)
-app = Flask(__name__, static_folder='../frontend/build', static_url_path = '../frontend/build')
+app = Flask(__name__, static_folder='../frontend/build', static_url_path = '/static')
 
 # Allow 
 CORS(app)
